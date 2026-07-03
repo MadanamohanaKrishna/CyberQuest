c3c790a94d08d48d737f4e1e8b5756e9c7d856a1
Madanamohana Krishna <ghostlyparth26@gmail.com>
2026-07-03 21:37:18 +0530

    feat: add centralized apllication confirguration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.version,
+    debug=settings.debug,
+)
 
 
 @app.get("/")
 def root():
     return {
-        "project": "CyberQuest",
-        "version": "1.0",
-        "status": "Sprint 0 Complete"
+        "project": settings.app_name,
+        "version": settings.version,
+        "status": "development"
     }
